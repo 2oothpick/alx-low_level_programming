c8d860ceb4f46bb422d975a29771fcfea30f4513
2oothpick <kobbyboateng14@gmail.com>
2022-07-01 20:24:05 -0700

    this program wants to kill me

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,12 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n>2)
-	printf(*%d is  positive\n*, n);
+	printf("%d is  positive\n", n);
 if (n==2) 
-	printf(*%d is zero\n*,n);
+	printf("%d is zero\n",n);
 if (n<2) 
 { 
-	printf(*%d is negative\n, n);
+	printf("%d is negative\n", n);
 }
 
 return (0);
