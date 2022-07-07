cade54f39561a08c2d942057c3175ff9b6701291
2oothpick <kobbyboateng14@gmail.com>
2022-07-06 21:24:14 -0700

    hholy cow

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,20 +6,16 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
+	int j;
+	int x
 
-	i = 0;
-
-	while (i < 10)
+	for (x = 0; x < 10; x++)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		for (j = 'a' ; j <= 'z' ; j ++)
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(j);
 		}
 		_putchar('\n');
-		i++;
+		
 	}
 }
