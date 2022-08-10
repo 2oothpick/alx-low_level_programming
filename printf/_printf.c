48a01061a08a507367afeb60fe56811a6744d740
2oothpick <kobbyboateng14@gmail.com>
2022-08-09 18:54:48 -0700

    all on me

--- printf/_printf.c
+++ printf/_printf.c
@@ -1,20 +1,23 @@
-#inclde "main.h"
+#include "main.h"
+
 /**
- * _printf - produces formated output
+ * _printf - produces output according to a format
  * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
  * Return: length of the formatted output string
  */
-
 int _printf(const char *format, ...)
 {
 	int (*pfunc)(va_list, flags_t *);
 	const char *p;
-	va_list ap;
+	va_list arguments;
 	flags_t flags = {0, 0, 0};
 
 	register int count = 0;
 
-	va_start(ap, format);
+	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
@@ -39,6 +42,6 @@ int _printf(const char *format, ...)
 			count += _putchar(*p);
 	}
 	_putchar(-1);
-	va_end(ap);
+	va_end(arguments);
 	return (count);
 }
