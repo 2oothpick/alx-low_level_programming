2078aa259629a31cb827762ac4a97a6058d6bb3b
2oothpick <kobbyboateng14@gmail.com>
2022-08-16 04:15:55 -0700

    strtcat code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,22 +4,19 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: string to add
- * Return: a pointer to the string
+ *
+ * Return: a pointer to the resulting string
  */
-
-
 char *_strcat(char *dest, char *src)
-{ 	
-/*find the length of dest string*/
-int len;
-for (len = 0; dest[len] != '\0'; len++)
-/*append src string from last dest string*/
-int i;
-for (i = 0; src[i] != '\0'; i++)
 {
-    dest[len] = src[i];
-    len++;
-}    
-dest[len] = '\0'; 
-return (dest);
+	int len, i;
+
+	for (len = 0; dest[len] != '\0'; len++)
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len] = src[i];
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
 }
