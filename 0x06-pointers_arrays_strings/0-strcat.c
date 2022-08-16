d215d87c782a5ae22be96b89c4550758a4b3cc9a
2oothpick <kobbyboateng14@gmail.com>
2022-08-15 23:19:09 -0700

    strtcat code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ * Return: a pointer to the string
+ */
+
 
 char *_strcat(char *dest, char *src)
+{ 	
+/*find the length of dest string*/
+int len;
+for (len = 0; dest[len] != '\0'; len++)
+/*append src string from last dest string*/
+int i;
+for (i = 0; src[i] != '\0'; i++)
 {
-    //find the length of dest string
-    int len;
-    for(len = 0; dest[len] != '\0'; len++){}
-    //append src string from last dest string
-    int i;
-    for (i = 0; src[i] != '\0'; i++)
-    {
-        dest[len] = src[i];
-        len++;
-    }    
-
-    return dest;
+    dest[len] = src[i];
+    len++;
+}    
+dest[len] = '\0'; 
+return (dest);
 }
