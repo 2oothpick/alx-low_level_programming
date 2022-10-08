d62c8f2b4ad2cf4bce1e8237b2b74e9fbd310df4
2oothpick <kobbyboateng14@gmail.com>
2022-10-08 04:54:11 +0000

    deleted

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P .. https://github.com/2oothpick/alx-low_level_programming/blob/b751fd17d046fa227e429b7fd21f50c924065602/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
